d1bfeaf4ad8855e3e518bcb118d66ca8a39480c0
Yasser-Alnajjar <yasseralnajjar@outlook.sa>
2023-08-11 03:51:12 +0300

    init

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ const middlewares = jsonServer.defaults();
 
 const PORT = 9000;
 app.db = router.db;
-app.use(
-  jsonServer.rewriter({
-    "/users": "/660/users",
-    "/users/:id": "/660/users/:id",
-    "/users?departmentId=:id": "/660/users?departmentId=:id",
-    "/departments": "/660/departments",
-    "/departments/:id": "/660/departments/:id",
-    "/branches": "/660/branches",
-    "/branches/:id": "/660/branches/:id",
-    "/tasks": "/660/tasks",
-    "/tasks/:id": "/660/tasks/:id",
-    "/employees": "/660/employees",
-    "/employees/:id": "/660/employees/:id",
-    "/attendance": "/660/attendance",
-    "/attendance/:id": "/660/attendance/:id",
-  })
-);
+const routes = {
+  "/users": "/660/users",
+  "/users/:id": "/660/users/:id",
+  "/users?department=:departmentName": "/660/users?department=:departmentName",
+  "/departments": "/660/departments",
+  "/departments/:id": "/660/departments/:id",
+  "/branches": "/660/branches",
+  "/branches/:id": "/660/branches/:id",
+  "/tasks": "/660/tasks",
+  "/tasks/:id": "/660/tasks/:id",
+  "/employees": "/660/employees",
+  "/employees/:id": "/660/employees/:id",
+  "/attendance": "/660/attendance",
+  "/attendance/:id": "/660/attendance/:id",
+};
+app.use(jsonServer.rewriter(routes));
 app.use(middlewares);
 app.use(auth);
 app.use(router);
 app.listen(PORT, () => {
   console.log(`Server is Running ${PORT}`);
+  console.log("Protected routes\n", routes);
 });
 module.exports = app;
